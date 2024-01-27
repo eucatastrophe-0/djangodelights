c9b10c6375d871d6fc5d6f6b5dabcf68f73a52d7
Sam Schock <83371366+eucatastrophe-0@users.noreply.github.com>
2024-01-27 14:52:38 -0500

    models.py created; database populated with instances for menu item, ingredients, and requirements

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Ingredient(models.Model):
+    name = models.CharField(max_length=255)
+    quantity = models.DecimalField(max_digits=5, decimal_places=2)
+    unit = models.CharField(max_length=255)
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+
+class MenuItem(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+class RecipeRequirement(models.Model):
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    quantity = models.DecimalField(max_digits=5, decimal_places=2)
+
+class Purchase(models.Model):
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField()
