aa3e84c6bd8e0e701d239b8b527026d8a8aa7463
Sam Schock <83371366+eucatastrophe-0@users.noreply.github.com>
2024-01-29 17:34:42 -0500

    began implementation of views, urls, templates, forms, static files; added Hello World homescreen

--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from .models import Ingredient, MenuItem, RecipeRequirement, Purchase
+from .forms import IngredientForm, MenuItemForm, RecipeRequirementForm, PurchaseForm
+from django.views.generic import TemplateView, ListView, CreateView, UpdateView, DeleteView
 
-# Create your views here.
+def inventory(request):
+  template = loader.get_template('base.html')
+  return HttpResponse(template.render())
